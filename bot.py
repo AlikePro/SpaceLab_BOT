7e74c004c9fb7aab19a0f24ed11a11592195ca1c
Alimzhan <67860180+AlikePro@users.noreply.github.com>
2026-01-05 00:10:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,8 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import WebAppInfo
-import asyncio
+from telegram import InlineKeyboardButton, WebAppInfo
 
-BOT_TOKEN = "8545772004:AAFSxxHOJ8saXcuZHaehrZCvD4XsWPQ_j68"
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-@dp.message(commands=["start"])
-async def start(message: types.Message):
-    kb = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text="🚀 Открыть Mini App",
-                    web_app=WebAppInfo(
-                        url="https://alikepro.github.io/SpaceLab_BOT/"
-                    )
-                )
-            ]
-        ]
+InlineKeyboardButton(
+    text="Open App",
+    web_app=WebAppInfo(
+        url="https://alikepro.github.io/SpaceLab_BOT/"
     )
-
-    await message.answer(
-        "Нажми кнопку ниже:",
-        reply_markup=kb
-    )
-
-async def main():
-    await dp.start_polling(bot)
-
-asyncio.run(main())
+)
